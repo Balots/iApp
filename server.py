74b5e54a3a512c27c5a8d3b971b6686c7585394d
iLaiDam <100014090+Balots@users.noreply.github.com>
2025-07-08 17:02:38 +0500

    Update server.py

--- server.py
+++ server.py
@@ -2,4 +2,4 @@ import uvicorn
 from route import *
 
 if __name__ == '__main__':
-    uvicorn.run('server:app', reload=True, host='192.168.1.95', port=8000)
+    uvicorn.run('server:app', reload=True, host='127.0.0.1', port=8000)
